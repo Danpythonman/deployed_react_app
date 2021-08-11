e5ac1ae99615e7d25514bc546f9234152241afe0
Daniel Di Giovanni <dannyjdigio@gmail.com>
2021-08-10 23:13:49 -0400

    Removed basic react page, added two hello world messages

---
+++ src/components/HelloWorldFunction.jsx
@@ -0,0 +1,12 @@
+const HelloWorldFunction = () => {
+    return (
+        <p>
+            This component was made with a function.
+            The "Hello World!" component was made with a class.
+            I was going to make one app with classes and another with functions,
+            but React took so long to install and initialize the project so I don't want to make another projcet.
+        </p>
+    );
+}
+
+export default HelloWorldFunction;
